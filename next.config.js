72ee6ae9670e8d756718cc1a82567e743053aadd
christhomp13 <theinfinitehumanblueprint@gmail.com>
2022-01-21 12:06:48 -0500

    first commit

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    images:{
+        domains: ['links.papareact.com']
+    }
+}
+// specifies which domains to accept images from
+// restart app since this is a configuration file
